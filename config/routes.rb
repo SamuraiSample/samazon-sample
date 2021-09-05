4a8884d55fa182f06564c7ed8800f1923bcf8523
samurai <rsaito@sejuku.net>
2021-09-05 05:07:27 +0000

    finish rails2_chap21

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     post "dashboard/login", :to => "admins/sessions#create"
     delete "dashboard/logout", :to => "admins/sessions#destroy"
   end
-  
+
   namespace :dashboard do
     resources :users, only: [:index, :destroy]
     resources :major_categories, except: [:new]
@@ -20,10 +20,11 @@ Rails.application.routes.draw do
         post "import/csv", :to => "products#import_csv"
         get  "import/csv_download", :to => "products#download_csv"
       end
-    end    
+    end
     resources :orders, only: [:index]
-  end  
-  
+  end
+
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
@@ -39,27 +40,29 @@ Rails.application.routes.draw do
     get "login", :to => "users/sessions#new"
     delete "logout", :to => "users/sessions#destroy"
   end
-  
+
   resource :users, only: [:edit, :update] do
     collection do
-      get "cart", :to => "shopping_carts#index"
-      post "cart/create", :to => "shopping_carts#create"
-      delete "cart", :to => "shopping_carts#destroy"
-      get "mypage", :to => "users#mypage"
-      get "mypage/edit", :to => "users#edit"
-      get "mypage/address/edit", :to => "users#edit_address"
-      put "mypage", :to => "users#update"
-      get "mypage/edit_password", :to =>"users#edit_password"
-      put "mypage/password", :to => "users#update_password" 
-      get  "mypage/favorite", :to => "users#favorite"
-      delete "mypage/delete", :to => "users#destroy"
+      get    "cart",               :to => "shopping_carts#index"
+      delete "cart",               :to => "shopping_carts#destroy"
+      post   "cart/create",        :to => "shopping_carts#create"
+      get    "mypage",                 :to => "users#mypage"
+      put    "mypage",                 :to => "users#update"
+      get    "mypage/edit",            :to => "users#edit"
+      get    "mypage/edit_password",   :to => "users#edit_password"
+      delete "mypage/delete",          :to => "users#destroy"
+      get    "mypage/address/edit",    :to => "users#edit_address"
+      put    "mypage/password",        :to => "users#update_password"
+      get    "mypage/favorite",        :to => "users#favorite"
+      get    "mypage/cart_history",    :to => "users#cart_history_index", :as => "mypage_cart_histories"
+      get    "mypage/cart_history/:num", :to => "users#cart_history_show", :as => "mypage_cart_history"
     end
-  end  
-  
+  end
+
   resources :products do
     member do
       get :favorite
-    end    
+    end
     resources :reviews, only: [:create]
   end
 end
